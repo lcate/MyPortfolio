3a86a4938ce5fc3f78e05aa365d5f868ec558855
lcate <golcevska.katerina@yahoo.com>
2020-09-23 20:11:12 +0200

    version1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+    $("#submit").click(function () {
+        console.log("clicked button")
+
+        var name=$("#name").val()
+        var email=$("#email").val()
+        var comment=$("#comment").val()
+
+
+
+    })
+})
